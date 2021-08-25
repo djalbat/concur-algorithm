9c7834ca61f4f8ab2ada9009025eb5271d51384b
James Smith <james.smith@djalbat.com>
2021-08-25 09:56:15 +0100

    Put back the example.

--- src/example.js
+++ src/example.js
@@ -7,16 +7,30 @@ import withStyle from "easy-with-style";  ///
 import { Body } from "easy";
 
 import View from "./example/view";
+import Agent from "./agent";
+import Document from "./example/document";
 
 const { renderStyles } = withStyle;
 
 const body = new Body(),
+      agent = Agent.fromNothing(),
       view =
 
-        <View/>
+        <View agent={agent} />
 
       ;
 
 renderStyles();
 
 body.mount(view);
+
+agent.initialise((content) => {
+  const richTextarea = view.getRichTextarea(),
+        document = Document.fromContentAndRichTextarea(content, richTextarea);
+
+  agent.setDocument(document);
+
+  view.activateRichTextarea();
+
+  view.setRichTextareaContent(content);
+});
